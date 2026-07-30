150f214f1e4f1b35c5808e51e78bbce27f4bc45f
WojtowiczSzymon <wojtow.szym@gmail.com>
2026-07-30 11:13:48 +0200

    independence case added for type explainer

--- src/shapiq/interaction_type_explainer.py
+++ src/shapiq/interaction_type_explainer.py
@@ -60,7 +60,7 @@ class TypeExplainer:
                 # print(value1, value2, values_combined)
                 if values_combined > -0.00001 and values_combined < 0.00001:
                     result.append("independence")
-                if self.sign(value1) == self.sign(value2) == self.sign(values_combined):
+                elif self.sign(value1) == self.sign(value2) == self.sign(values_combined):
                     result.append("synergy")
                 elif self.index == "Rred" and valuesrred[interaction_index] > 0:
                     result.append("redundancy")
