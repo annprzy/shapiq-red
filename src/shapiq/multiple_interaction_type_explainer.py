1630af2701ec43f4e5455b6a289e32abc14cf6ba
WojtowiczSzymon <wojtow.szym@gmail.com>
2026-08-05 15:19:24 +0200

    o information based type detection

--- src/shapiq/multiple_interaction_type_explainer.py
+++ src/shapiq/multiple_interaction_type_explainer.py
@@ -3,13 +3,14 @@ module explaining the interaction types
 """
 import numpy as np
 from shapiq import TabularExplainer
+from scipy.stats import norm, rankdata
+from scipy.stats import gaussian_kde
 
 
 class TypeExplainer:
     """
     interaction type explainer class
     """
-
     def __init__(self, x, model, sample_data, index="RI", max_order=2, budget=256):
         self.x = x
         self.model = model
@@ -17,6 +18,114 @@ class TypeExplainer:
         self.index = index
         self.budget = budget
         self.max_order = max_order
+    def prepare_data(self, data, model, x, trials):
+        """
+        prepare data for o-information computation
+        """
+        data_with_y = []
+        ind = -1
+        
+        for i in range(len(data)):
+            if ind == -1 and (x == data[i]).all():
+                ind = i
+            data_with_y.append(np.append(data[i], np.array(model(np.array([data[i]]))[0])))
+        if ind == -1:
+            ind = len(data_with_y) - 1
+            data_with_y.append(np.append(x, np.array(model(np.array([x]))[0])))
+        s=len(data_with_y)
+        for i in range(s):
+            for j in range(trials):
+                data_with_y.append(data_with_y[i])
+        data_with_y = np.array(data_with_y)
+
+        return ind, data_with_y
+
+    def o_information(self, x, model, data, coalition, trials=100):
+        """
+        compute o-information of a given input
+        """
+        coalition = list(coalition)
+        ind,prepared_data = self.prepare_data(data, model, x, trials)
+        n=len(x)
+        to_delete = []
+        for i in range(n):
+            if i not in coalition:
+                to_delete.append(i-1)
+        prepared_data = np.delete(prepared_data, to_delete, axis=1)
+        x = np.delete(x, to_delete, axis=0)
+        oinfo = self.continuous_local_oinfo(prepared_data)
+        return oinfo[ind]
+    def copula_transform(self,  X: np.ndarray) -> np.ndarray:
+        """Transforms continuous/clustered feature columns into standard Gaussian distributions
+
+        (N(0,1)) using empirical probability integral transform (Copula).
+        This normalizes 1D, 3D, and 4D subspace scaling for KDE density estimation.
+        """
+        N, d = X.shape
+        X_gauss = np.zeros_like(X)
+        for col in range(d):
+            ranks = (rankdata(X[:, col], method="average") - 0.5) / N
+            X_gauss[:, col] = norm.ppf(ranks)
+        return X_gauss
+    def continuous_local_oinfo(
+        self, X: np.ndarray, bw_method: float = 0.4
+    ) -> np.ndarray:
+        """Computes Local O-information for continuous features using Gaussian Copula + KDE."""
+        X_norm = self.copula_transform(X)
+
+        N, n_features = X_norm.shape
+
+        kde_full = gaussian_kde(X_norm.T, bw_method=bw_method)
+        i_full = -kde_full.logpdf(X_norm.T)
+
+        i_indiv = np.zeros(N)
+        for j in range(n_features):
+            kde_j = gaussian_kde(X_norm[:, j : j + 1].T, bw_method=bw_method)
+            i_indiv += -kde_j.logpdf(X_norm[:, j : j + 1].T)
+
+        i_leave_one_out = np.zeros(N)
+        for j in range(n_features):
+            subset = np.delete(X_norm, j, axis=1)
+            kde_sub = gaussian_kde(subset.T, bw_method=bw_method)
+            i_leave_one_out += -kde_sub.logpdf(subset.T)
+        return (n_features - 2) * i_full + i_indiv - i_leave_one_out
+    def copula_transform(self, X: np.ndarray) -> np.ndarray:
+        """Transforms data to standard normal marginals via empirical copula
+
+        to stabilize k-NN density estimation on clustered/jittered data.
+        """
+        N, d = X.shape
+        X_norm = np.zeros_like(X)
+        for col in range(d):
+            ranks = rankdata(X[:, col], method="average") / (N + 1.0)
+            X_norm[:, col] = norm.ppf(ranks)
+        return X_norm
+    def predict_type(self, coalition=None):
+        """
+        predict interaction type of a given input
+        """
+        if coalition is None:
+            coalition = tuple(range(len(self.x)))
+        x = self.x
+        model = self.model
+        data = self.data
+        budget = self.budget
+        oinfo = self.o_information(x, model, data, coalition)
+        if oinfo > 0:
+            return "redundancy"
+        elif oinfo < 0:
+            explainer = TabularExplainer(
+                model=model,
+                data=data,
+                index="k-SII",
+                max_order=len(x),
+            ).explain(x, budget=budget)
+        for i in list(coalition):
+            if self.sign(explainer[i+1]) != self.sign(explainer.dict_values[coalition]):
+                return "antagonism"
+        return "synergy"
+
+    
 
     def sign(self, a, margin=0.001):
         """
@@ -25,69 +134,3 @@ class TypeExplainer:
         if a + margin > 0:
             return 1
         return -1
-
-    def explain(self):
-        """
-        explain interaction type by returing an array of their names
-        """
-        n = len(self.x)
-        explainersii = TabularExplainer(
-            model=self.model,
-            data=self.data,
-            index="k-SII",
-            max_order=self.max_order,
-            normalize=False,
-            sample_size=len(self.data),
-        )
-
-        valuessii = explainersii.explain(self.x, budget=self.budget)
-
-        explainerrred = TabularExplainer(
-            model=self.model,
-            data=self.data,
-            index=self.index,
-            max_order=self.max_order,
-            normalize=False,
-            sample_size=len(self.data),
-        )
-        valuesrred = explainerrred.explain(self.x, budget=self.budget)
-        result = []
-        for key, values_combined in valuessii.dict_values.items():
-            lista = list(key)
-            if(len(lista) > 1):
-                singular_result = {
-                    "interaction name": key,
-                    "synergy": True,
-                    "full independence": False,
-                    "redundancy": 0,
-                    "antagonism": 0,
-                    "partial independence": 0,
-                }
-                if values_combined < 0.00001 and values_combined > -0.00001:
-                    singular_result["full independence"] = True
-                    singular_result["synergy"] = False
-                    result.append(singular_result)
-                else:
-                    for i in lista:
-                        if self.sign(valuessii[tuple([i])]) != self.sign(valuessii[key]):
-                            singular_result["synergy"] = False
-                    if singular_result["synergy"]:
-                        result.append(singular_result)
-                    else:
-                        for i in lista:
-                            rest = lista.copy()
-                            rest.remove(i)
-                            rest = tuple(rest)
-                            value1 = valuessii[tuple([i])]
-                            value2 = valuessii[rest]
-                            #print(value1, value2, values_combined)
-                            if values_combined > -0.00001 and values_combined < 0.00001:
-                                singular_result["partial independence"] += 1
-                            elif self.index == "Rred" and valuesrred[key] > 0:
-                                singular_result["redundancy"] += 1
-                            elif self.index == "RI" and values_combined/(values_combined+value1+value2) < 0:
-                                singular_result["redundancy"] += 1
-                            else:
-                                singular_result["antagonism"] += 1
-                        result.append(singular_result)
-        return result
