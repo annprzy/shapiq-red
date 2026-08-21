533f5a9124061f75e994011dc5355288e420d5aa
WojtowiczSzymon <wojtow.szym@gmail.com>
2026-08-21 17:15:55 +0200

    changed native multi feature main effect calculation to a groupped one

--- src/shapiq/interaction_type_explainer.py
+++ src/shapiq/interaction_type_explainer.py
@@ -5,6 +5,8 @@ from xml.parsers.expat import model
 
 import numpy as np
 from shapiq import TabularExplainer
+from shapiq.imputer import MarginalImputer
+from shapiq.approximator import PermutationSamplingSII
 import itertools
 
 
@@ -13,7 +15,7 @@ class TypeExplainer:
     interaction type explainer class
     """
 
-    def __init__(self, x, model, sample_data, index="RI", budget=256):
+    def __init__(self, x, model, sample_data, index="RI", budget=1000):
         self.x = x
         self.model = model
         self.data = sample_data
@@ -41,46 +43,63 @@ class TypeExplainer:
         """
         explain interaction between two coalitions
         """
-        n = len(self.x)
+        def create_grouped_game(base_value_function, groups: list[list[int]], n_features: int):
+            listed_indices = {idx for g in groups for idx in g}
+            full_groups = [list(g) for g in groups] + [
+                [idx] for idx in range(n_features) if idx not in listed_indices
+            ]
 
-        explainersii = TabularExplainer(
-            model=self.model,
-            data=self.data,
+            def grouped_game(coalitions_groups: np.ndarray) -> np.ndarray:
+                is_1d = coalitions_groups.ndim == 1
+                if is_1d:
+                    coalitions_groups = np.atleast_2d(coalitions_groups)
+
+                n_coalitions = coalitions_groups.shape[0]
+                coalitions_features = np.zeros((n_coalitions, n_features), dtype=bool)
+
+                for group_idx, feature_indices in enumerate(full_groups):
+                    coalitions_features[:, feature_indices] = coalitions_groups[:, group_idx : group_idx + 1]
+
+                res = base_value_function(coalitions_features)
+                if is_1d and isinstance(res, np.ndarray):
+                    return res.ravel()
+                return res
+
+            return grouped_game, full_groups
+
+        imputer = MarginalImputer(model=self.model, data=self.data, random_state=42)
+        imputer.fit(self.x)
+        base_value_function = imputer.value_function
+        n_features = self.data.shape[1]
+
+        my_groups = [list(coalition1), list(coalition2)]
+
+        grouped_game, full_groups = create_grouped_game(
+            base_value_function=base_value_function,
+            groups=my_groups,
+            n_features=n_features
+        )
+        approximator = PermutationSamplingSII(
+            n=len(full_groups), 
+            max_order=max(len(coalition1), len(coalition2)), 
             index="k-SII",
-            max_order=n,
-            normalize=False,
-            sample_size=len(self.data),
-        ).explain(self.x, budget=self.budget)
+            random_state=42,
+        )
 
-        value1 = self.calculate_main_effect(coalition1, explainersii)
-        value2 = self.calculate_main_effect(coalition2, explainersii)
+        explainersii = approximator.approximate(budget=self.budget, game=grouped_game)
 
-        valuesri = TabularExplainer(
-            model=self.model,
-            data=self.data,
-            index=self.index,
-            max_order=n,
-            normalize=False,
-            sample_size=len(self.data),
-        ).explain(self.x, budget=self.budget)
 
-        mix = tuple(set(coalition1).union(set(coalition2)))
-        values_combined = explainersii.dict_values[mix]
+        value1 = explainersii.dict_values[(0,)]
+        value2 = explainersii.dict_values[(1,)]
+        #print(explainersii.dict_values)
+        values_combined = explainersii.dict_values[(0, 1)]
 
         RI = values_combined / (value1 + value2 + values_combined) if (value1 + value2 + values_combined) != 0 else 0
-        #print("RI:", RI)
-        #print(value1, value2, values_combined)
         
-        synergy = True
-        for i in mix:
-            if self.sign(explainersii.dict_values[tuple([i])]) != self.sign(values_combined):
-                synergy = False
-        if values_combined > -0.00001 and values_combined < 0.00001:
-            return "independence"
-        elif synergy and self.sign(value1) == self.sign(value2) == self.sign(values_combined):
+        if self.sign(value1) == self.sign(value2) == self.sign(values_combined):
             return "synergy"
-        elif self.index == "Rred" and valuesri.dict_values[mix] > 0:
-            return "redundancy"
+        elif values_combined > -0.00001 and values_combined < 0.00001:
+            return "independence"
         elif self.index == "RI" and RI < 0:
             return "redundancy"
         else:
